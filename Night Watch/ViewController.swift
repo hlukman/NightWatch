b2c7befd482c2f6301c6efa2a60263601f5c21b2
Handy Lukman <handy@lukman.com>
2019-05-24 08:41:40 -0400

    Initial Commit

---
+++ Night Watch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Night Watch
+//
+//  Created by Handy Lukman on 5/24/19.
+//  Copyright © 2019 Handy Lukman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
